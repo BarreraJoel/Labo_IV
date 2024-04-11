0153174a68a3a18108997c8a85c5c120e61982f4
BarreraJoel <joelmanuelbar@gmail.com>
2024-04-11 11:49:36 -0300

    Ejercicio 1 completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clase_1';
+  protected edadUno: string = "";
+  protected edadDos: string = "";
+  protected promedio: number = 0;
+  protected suma: number = 0;
+
+  protected Calcular(): void {
+    this.suma = parseInt(this.edadUno) + parseInt(this.edadDos);
+    this.promedio = this.suma / 2;
+  }
+  
+  protected Limpiar(): void {
+    this.edadUno = "";
+    this.edadDos = "";
+    this.promedio = 0;
+    this.suma = 0;
+  }
+
 }
